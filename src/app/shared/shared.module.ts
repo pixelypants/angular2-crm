d176e7b37d103c465bfd91140112e04998c49846
pixelypants <hudson@pixelypants.com>
2016-11-23 20:42:31 +1000

    Add foundation modules to application

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
+@NgModule({
+    imports: [
+        CommonModule,
+        RouterModule,
+        HttpModule,
+        FormsModule,
+        ReactiveFormsModule,
+    ],
+    exports: [
+        CommonModule,
+        RouterModule,
+        FormsModule,
+        ReactiveFormsModule,
+    ],
+    declarations: [],
+    providers: [],
+})
+export class SharedModule { }
